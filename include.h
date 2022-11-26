a6bf5151b4811f38a4467d5c10a25e83ab94dd82
gembright stone hung <34207794+gemhung@users.noreply.github.com>
2022-11-27 01:54:53 +0900

    Update include.h

--- include.h
+++ include.h
@@ -25,6 +25,73 @@ using vec = vector<T>; // Ex: vec<std::pair<int, int>
 #define all(v) v.begin(), v.end()
 #define mn(v) const int m = v.size(), n = v[0].size()
 
+============== binary index tree ================
+constexpr int static max_n = 50001;
+long long bt[max_n + 1] = {};
+long long prefix_sum(int i){
+    long long sum = 0;
+    for (i = i + 1; i > 0; i -= i & (-i))
+        sum += bt[i];
+    return sum;
+}
+void add(int i, int val){
+    for (i = i + 1; i <= max_n; i += i & (-i))
+        bt[i] += val;
+} 
+
+
+==================================================
+Merge sort technique
+
+    void help(vector<int>& v,  int l, int r){
+        if(l+1 == r)
+            return;
+        auto mid = l + (r-l)/2;
+        
+        help(v, l, mid);
+        help(v, mid, r);
+
+        for(auto i = l, j=mid ; i< mid && j < r;){
+		   // todo
+            for ( ;i<mid && v[i] <= (long long)2*v[j]; i++);
+            if(i == mid)
+                break;
+            
+            ret += mid-i;
+            j++;
+        }
+        
+        std::inplace_merge(v.begin()+l, v.begin()+mid, v.begin()+r);
+    }
+
+=======================
+ 
+long largest_power(long N) {
+    //changing all right side bits to 1.
+    N = N | (N>>1);
+    N = N | (N>>2);
+    N = N | (N>>4);
+    N = N | (N>>8);
+    N = N | (N>>16);
+    return (N+1)>>1;
+}
+
+
+=======================
+
+	•	Set union A | B
+	•	Set intersection A & B
+	•	Set subtraction A & ~B
+	•	Set negation ALL_BITS ^ A or ~A
+	•	Set bit A |= 1 << bit
+	•	Clear bit A &= ~(1 << bit)
+	•	Test bit (A & 1 << bit) != 0
+	•	Extract last bit A&-A or A&~(A-1) or x^(x&(x-1))
+	•	Remove last bit A&(A-1)
+	•	Get all 1-bits ~0
+
+
+========================
 #include <string>
 #include <vector>
 #include <algorithm>
