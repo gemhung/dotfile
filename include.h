44c104fd0d2c2d4df093beb0110632ee8592b702
gembright stone hung <34207794+gemhung@users.noreply.github.com>
2024-05-04 18:08:22 +0900

    Update include.h

--- include.h
+++ include.h
@@ -158,10 +158,12 @@ class DSU {
 private:
     vector<int> v, rank, count;
 public:
+    int group_size;
     DSU(int n): 
         v(vector<int>(n, -1)),
         rank(vector<int>(n, -1)),
-        count(vector<int>(n, 1))
+        count(vector<int>(n, 1)),
+        group_size(n)
     {
         // empty constructor
     }
@@ -186,10 +188,14 @@ public:
             v[r2] = r1; 
         if (rank[r1] == rank[r2]) 
             ++rank[r1];
+        group_size--;
     }
     // how many verticles in a group in which x is
     int size(int x) {
         return count[find(x)];
+    }
+    int groups() {
+        return this->group_size;
     }
 };
 // ================ split string with ' ' into vector<string> ==================
