ea107adaaa2a8595ea0a85c10d28ff40a6b0c7f5
gembright stone hung <34207794+gemhung@users.noreply.github.com>
2024-06-25 09:13:27 +0900

    Update include.h

--- include.h
+++ include.h
@@ -59,6 +59,26 @@ template<typename T> bool chkmax(T &x, T y) { return (x > y) ? x = y, 1 : 0; }
 #define N(v) const int n = v.size()
 #define MN(v) const int m = v.size(), n = v[0].size()
 
+// ================ merging, interval ==================
+class CountIntervals {
+public:
+    map<int, int> m;
+    //int cnt = 0;
+    void add(int left, int right) {
+        auto it = m.upper_bound(left);
+        if (it != begin(m) && prev(it)->second >= left)
+            it = prev(it);
+        for (; it != end(m) && it->first <= right; m.erase(it++)) {
+            left = min(left, it->first);
+            right = max(right, it->second);
+            cnt -= it->second - it->first + 1;
+        }
+        //cnt += right - left + 1;
+        m[left] = right;
+    }
+    //int count() { return cnt; }
+};
+
 // ================ find all factors
     vector<int> help(int n)
     {
